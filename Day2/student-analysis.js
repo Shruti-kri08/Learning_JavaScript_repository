492d5f9a2bf03bf0ca2359bedb87c4a9bc32867a
Shruti Kumari <shrutiikumarii08@gmail.com>
2026-03-05 13:43:01 +0530

    Practice array and object operations with student data

--- Day2/student-analysis.js
+++ Day2/student-analysis.js
@@ -9,8 +9,9 @@ students.forEach((val)=>{
     console.log(val.name)
 })
 
-const newStu=[]
-const passStu=[]
+const newStu=[];
+const passStu=[];
+let classTotalMarks=0;
 students.forEach((val)=>{
     let totalmarks=0,avg=0
     val.marks.forEach((e)=>{
@@ -18,24 +19,17 @@ students.forEach((val)=>{
         
     })
     avg=totalmarks/val.marks.length
+     classTotalMarks+=avg
+
     newStu.push({ name:val.name ,average:avg})
+    
     if(avg>=60){
         passStu.push({ name:val.name ,average:avg})
     }
 
 })
 console.log(newStu);
-console.log(passStu);
+console.log("Pass students:" ,passStu);
 
-let classTotalMarks=0;
-students.forEach((val)=>{
-    let totalMarks=0;
-    val.marks.forEach((e)=>{
-        totalMarks+=e
-    })
-    let avg=totalMarks/val.marks.length
-    classTotalMarks+=avg
-
-})
 let classAvgMarks=classTotalMarks/students.length
-console.log(classAvgMarks);
+console.log("Average Marks of class :" ,classAvgMarks);
